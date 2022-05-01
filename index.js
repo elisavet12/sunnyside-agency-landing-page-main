f0710c6de3bf249669e2a10be91b1126af3af377
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-05-01 22:43:27 +0300

    ...

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ var burger = document.querySelector("button.burger");
 var nav = document.querySelector("nav.nav--main");
 var body = document.querySelector("body");
 var mediaQuery = window.matchMedia("(min-width: 600px)");
+var myDiv = document.querySelector("div.test");
 
 function openMenu() {
-  nav.classList.add("nav--open");
-  body.classList.add("noScroll");
-  document.getElementById("myDiv").style.display = "none";
+  nav.classList.toggle("nav--open");
+  body.classList.toggle("noScroll");
+  myDiv.classList.toggle("test--open");
+
+  // document.getElementById("myDiv").style.display = "none";
 }
 
 function isMobile() {
